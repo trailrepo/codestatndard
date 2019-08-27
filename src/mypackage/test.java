265fa2669d2b28cd0112e5836530d683c6b71d26
nareshbabur <nareshbabur@GCDTP004.GADAGET.COM>
2019-08-27 16:28:20 +0530

    fi

---
+++ src/mypackage/test.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class test {
+	public static void main(String[] args) {
+		String a1=null;
+		System.out.println(a1.length());
+		boolean a=true;
+		while (a) {
+			
+		}
+	}
+}
